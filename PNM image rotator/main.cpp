619683e5d8e7b01ba092dc7faef319f4e5c5c195
koalaa13 <nooshe981@gmail.com>
2020-02-21 18:52:18 +0300

    Rewrite on normal code-style

--- PNM image rotator/main.cpp
+++ PNM image rotator/main.cpp
@@ -4,172 +4,177 @@
 
 typedef unsigned char byte;
 
-int PIXEL_SIZE;
+void printError(const std::string &msg) {
+    fprintf(stderr, "%s", msg.c_str());
+    exit(0);
+}
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        throw std::runtime_error("You should enter input filename, output filename and type of conversion");
-    }
-    char *input_filename = argv[1];
-    char *output_filename = argv[2];
-    int conversion_type = -1;
-    try {
-        conversion_type = std::stoi(argv[3]);
-    } catch (std::invalid_argument const &e) {
-        throw std::runtime_error("Conversion type should be a number");
-    } catch (std::out_of_range const &e) {
-        throw std::runtime_error("Conversion type is too big");
-    }
-    if (conversion_type < 0 || conversion_type > 4) {
-        throw std::runtime_error("Conversion type should be in [0-4]");
+struct ImageFile {
+    explicit ImageFile(char *filename, const char *mode) {
+        data = fopen(filename, mode);
+        if (data == nullptr) {
+            printError("Can't open an image file");
+        }
     }
 
-    FILE *image_file = fopen(input_filename, "rb");
-    if (image_file == nullptr) {
-        throw std::runtime_error("Can't open an image");
+    ~ImageFile() {
+        fclose(data);
     }
-    char format, trash;
-    int image_type = 0, width = 0, height = 0, size = 0;
-    int readed = fscanf(image_file, "%c%d%d%d%d%c", &format, &image_type, &width, &height, &size, &trash);
-    if (readed != 6 || format != 'P' || image_type > 6 || image_type < 5 || width <= 0 || height <= 0 || size != 255) {
-        fclose(image_file);
-        throw std::runtime_error("Incorrect image format");
-    }
-    if (image_type == 5) {
-        PIXEL_SIZE = 1;
-    } else {
-        PIXEL_SIZE = 3;
-    }
-    int read_count = PIXEL_SIZE * width * height;
-    byte *image = (byte *) malloc(sizeof(byte) * read_count);
-    if (image == nullptr) {
-        fclose(image_file);
-        throw std::runtime_error("Not enough memory");
-    }
-    readed = fread(image, 1, read_count, image_file);
-    if (readed != read_count) {
-        free(image);
-        fclose(image_file);
-        throw std::runtime_error("Error while trying to read an image");
-    }
-    fclose(image_file);
 
-    FILE *output_file = fopen(output_filename, "wb");
-    if (conversion_type == 0) {
-        fprintf(output_file, "P%d\n%d %d\n%d\n", image_type, width, height, size);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
+    FILE *getData() const {
+        return data;
+    }
+
+private:
+    FILE *data;
+};
+
+struct Image {
+    explicit Image(const ImageFile &imageFile) {
+        data = nullptr;
+        char format, trash;
+        int imageType;
+        int readed = fscanf(imageFile.getData(), "%c%d%d%d%d%c", &format, &imageType, &width, &height, &size, &trash);
+        if (readed != 6 || format != 'P' || imageType > 6 || imageType < 5 || width <= 0 || height <= 0 ||
+            size != 255) {
+            printError("Incorrect image format");
         }
-        for (int i = 0; i < readed; ++i) {
-            image[i] = ~image[i];
+        PIXEL_SIZE = (imageType == 5) ? 1 : 3;
+        IMAGE_SIZE = PIXEL_SIZE * width * height;
+        data = (byte *) malloc(sizeof(byte) * IMAGE_SIZE);
+        if (data == nullptr) {
+            printError("Not enough memory to open image");
         }
-        fwrite(image, sizeof(byte), readed, output_file);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
+        readed = fread(data, 1, IMAGE_SIZE, imageFile.getData());
+        if (readed != IMAGE_SIZE) {
+            printError("Error while trying to read an image");
         }
     }
-    if (conversion_type == 1) {
-        fprintf(output_file, "P%d\n%d %d\n%d\n", image_type, width, height, size);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
+
+    ~Image() {
+        if (data != nullptr) {
+            free(data);
         }
+    }
+
+    void inversion() {
+        for (int i = 0; i < IMAGE_SIZE; ++i) {
+            data[i] = ~data[i];
+        }
+    }
+
+    void horizontal() {
         for (int i = 0, j = height - 1; i < j; ++i, --j) {
             for (int k = 0; k < width; ++k) {
                 int f = k * PIXEL_SIZE + i * width * PIXEL_SIZE;
                 int s = k * PIXEL_SIZE + j * width * PIXEL_SIZE;
                 for (int z = 0; z < PIXEL_SIZE; ++z) {
-                    std::swap(image[f + z], image[s + z]);
+                    std::swap(data[f + z], data[s + z]);
                 }
             }
         }
-        fwrite(image, sizeof(byte), readed, output_file);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
-        }
     }
-    if (conversion_type == 2) {
-        fprintf(output_file, "P%d\n%d %d\n%d\n", image_type, width, height, size);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
-        }
+
+    void vertical() {
         for (int i = 0; i < height; ++i) {
             for (int j = 0, k = width - 1; j < k; ++j, --k) {
                 int f = j * PIXEL_SIZE + i * width * PIXEL_SIZE;
                 int s = k * PIXEL_SIZE + i * width * PIXEL_SIZE;
                 for (int z = 0; z < PIXEL_SIZE; ++z) {
-                    std::swap(image[f + z], image[s + z]);
+                    std::swap(data[f + z], data[s + z]);
                 }
             }
         }
-        fwrite(image, sizeof(byte), readed, output_file);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
-        }
     }
-    if (conversion_type == 3) {
-        fprintf(output_file, "P%d\n%d %d\n%d\n", image_type, height, width, size);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
+
+    void clockwise() {
+        byte *buff = (byte *) malloc(sizeof(byte) * IMAGE_SIZE);
+        if (buff == nullptr) {
+            printError("Out of memory while converting");
         }
-        byte *image_res = (byte *) malloc(sizeof(byte) * read_count);
-        if (image_res == nullptr) {
-            free(image);
-            throw std::runtime_error("Out of memory while converting");
-        }
-        int p = 0;
+        size_t p = 0;
         for (int i = 0; i < width; ++i) {
             for (int j = height - 1; j >= 0; --j) {
                 int f = i * PIXEL_SIZE + j * width * PIXEL_SIZE;
                 for (int z = 0; z < PIXEL_SIZE; ++z) {
-                    image_res[p++] = image[f + z];
+                    buff[p++] = data[f + z];
                 }
             }
         }
-        fwrite(image_res, sizeof(byte), readed, output_file);
-        if (ferror(output_file)) {
-            free(image);
-            free(image_res);
-            throw std::runtime_error("Something happened while converting");
-        }
-        free(image_res);
+        std::swap(data, buff);
+        std::swap(width, height);
+        free(buff);
     }
-    if (conversion_type == 4) {
-        fprintf(output_file, "P%d\n%d %d\n%d\n", image_type, height, width, size);
-        if (ferror(output_file)) {
-            free(image);
-            throw std::runtime_error("Something happened while converting");
+
+    void counterClockwise() {
+        byte *buff = (byte *) malloc(sizeof(byte) * IMAGE_SIZE);
+        if (buff == nullptr) {
+            printError("Out of memory while converting");
         }
-        byte *image_res = (byte *) malloc(sizeof(byte) * read_count);
-        if (image_res == nullptr) {
-            free(image);
-            throw std::runtime_error("Out of memory while converting");
-        }
-        int p = 0;
+        size_t p = 0;
         for (int i = width - 1; i >= 0; --i) {
             for (int j = 0; j < height; ++j) {
                 int f = i * PIXEL_SIZE + j * width * PIXEL_SIZE;
                 for (int z = 0; z < PIXEL_SIZE; ++z) {
-                    image_res[p++] = image[f + z];
+                    buff[p++] = data[f + z];
                 }
             }
         }
-        fwrite(image_res, sizeof(byte), readed, output_file);
-        if (ferror(output_file)) {
-            free(image);
-            free(image_res);
-            throw std::runtime_error("Something happened while converting");
-        }
-        free(image_res);
+        std::swap(data, buff);
+        std::swap(height, width);
+        free(buff);
     }
-    fclose(output_file);
-    free(image);
 
+    void writeToFile(const ImageFile &imageFile) {
+        fprintf(imageFile.getData(), "P%d\n%d %d\n%d\n", PIXEL_SIZE == 1 ? 5 : 6, width, height, size);
+        if (ferror(imageFile.getData())) {
+            printError("Something happened while writing result to output file");
+        }
+        fwrite(data, sizeof(byte), IMAGE_SIZE, imageFile.getData());
+        if (ferror(imageFile.getData())) {
+            printError("Something happened while converting");
+        }
+    }
+
+private:
+    int width, height, size, PIXEL_SIZE, IMAGE_SIZE;
+    byte *data;
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printError("You should enter input filename, output filename and type of conversion");
+    }
+    int conversionType = -1;
+    try {
+        conversionType = std::stoi(argv[3]);
+    } catch (std::invalid_argument const &e) {
+        printError("Conversion type should be a number");
+    } catch (std::out_of_range const &e) {
+        printError("Conversion type is too big");
+    }
+    if (conversionType < 0 || conversionType > 4) {
+        printError("Conversion type should be in [0-4]");
+    }
+
+    ImageFile imageFile(argv[1], "rb");
+    Image image(imageFile);
+    ImageFile resultImageFile(argv[2], "wb");
+    if (conversionType == 0) {
+        image.inversion();
+    }
+    if (conversionType == 1) {
+        image.horizontal();
+    }
+    if (conversionType == 2) {
+        image.vertical();
+    }
+    if (conversionType == 3) {
+        image.clockwise();
+    }
+    if (conversionType == 4) {
+        image.counterClockwise();
+    }
+    image.writeToFile(resultImageFile);
     return 0;
 }
